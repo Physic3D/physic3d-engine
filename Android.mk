02ed85ca81117876cd098d06d8d0d2870f21b0fe
Nenquen <nenquen@users.noreply.github.com>
2026-06-17 23:15:47 +0300

    Fix Android NDK build: NanoGL include, mainui/vgui paths

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #include $(call all-subdir-makefiles)
 
 include $(XASH3D_PATH)/engine/Android.mk
-include $(XASH3D_PATH)/mainui/Android.mk
-include $(XASH3D_PATH)/vgui_support/Android.mk
+include $(XASH3D_PATH)/engine/mainui/Android.mk
+include $(XASH3D_PATH)/engine/vgui_support/Android.mk
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,1 +1,2 @@
-include $(call my-dir)/../Android.mk
+include $(XASH3D_PATH)/Android.mk
+include $(NANOGL_PATH)/Android.mk
