cc7f8c456dc1d5aa656c9d41fd9aaa87982b25d8
Physic3D Developer <physic3d-dev@users.noreply.github.com>
2026-06-23 01:11:28 +0300

    Fix vid_android.cpp: cast GLubyte* to char* for C++ compat

--- engine/platform/android/vid_android.cpp
+++ engine/platform/android/vid_android.cpp
@@ -56,10 +56,10 @@ void GL_InitExtensions( void )
 	GL_SetExtension( GL_OPENGL_110, true );
 
 	// get our various GL strings
-	glConfig.vendor_string = pglGetString( GL_VENDOR );
-	glConfig.renderer_string = pglGetString( GL_RENDERER );
-	glConfig.version_string = pglGetString( GL_VERSION );
-	glConfig.extensions_string = pglGetString( GL_EXTENSIONS );
+	glConfig.vendor_string = (const char*)pglGetString( GL_VENDOR );
+	glConfig.renderer_string = (const char*)pglGetString( GL_RENDERER );
+	glConfig.version_string = (const char*)pglGetString( GL_VERSION );
+	glConfig.extensions_string = (const char*)pglGetString( GL_EXTENSIONS );
 	MsgDev( D_INFO, "Video: %s\n", glConfig.renderer_string );
 
 	// initalize until base opengl functions loaded
